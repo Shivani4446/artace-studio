f360b6695e25f4174f62d5046ca05139ca6ece07
sahilmahalley-tech <sahilmahalley@gmail.com>
2026-06-09 22:44:09 +0530

    Added edge runtime

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
-export const runtime = 'edge';
 const ArticleLayout = dynamic(() =>
   import("@/components/article/ArticleLayout").then((mod) => mod.default)
 );
