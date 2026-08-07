17a3c53db17f8df46c4bc21272c52fdfe8167f93
sahilmahalley-pere-tech <sahil.mahalley@perennialsys.com>
2026-08-07 17:27:00 +0530

    Added trade page

--- components/footer.tsx
+++ components/footer.tsx
@@ -29,6 +29,7 @@ const footerSections: FooterSection[] = [
       { label: "Shop", href: "/shop" },
       { label: "Art Rentals", href: "/rentals" },
       { label: "Corporate & Bulk Orders", href: "/corporate-bulk-orders" },
+      { label: "Trade Program", href: "/trade" },
       { label: "Cart", href: "/cart" },
       { label: "Track Your Order", href: "/dashboard/orders" },
       { label: "Wishlist", href: "/wishlist" },
